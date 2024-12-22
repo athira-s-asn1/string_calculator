18da891c922ed21038615c9a63291c8b6ba19d52
athira-s-asn1 <asnathira@gmail.com>
2024-12-22 19:18:05 +0530

    code Refactor

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,35 +1,47 @@
 class StringCalculator
+  DEFAULT_DELIMITER = /[\n,]/
+
   def add(numbers)
     return 0 if numbers.empty?
 
-    delimiter = /[\n,]/
-    
-    # Check for custom delimiter
-    if numbers.start_with?('//')
-      custom_delimiter, numbers = parse_custom_delimiter(numbers)
-      delimiter = Regexp.new(custom_delimiter)
-    end
+    delimiter, numbers = extract_delimiter_and_numbers(numbers)
+    nums = parse_numbers(numbers, delimiter)
 
-    nums = numbers.split(delimiter).map(&:to_i)
-    negatives = nums.select(&:negative?)
-
-    raise "negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
-
-    # Ignore numbers greater than 1000
-    nums.reject { |n| n > 1000 }.sum
+    validate_negatives(nums)
+    filter_large_numbers(nums).sum
   end
 
   private
 
+  def extract_delimiter_and_numbers(numbers)
+    if numbers.start_with?('//')
+      parse_custom_delimiter(numbers)
+    else
+      [DEFAULT_DELIMITER, numbers]
+    end
+  end
+
+  def parse_numbers(numbers, delimiter)
+    numbers.split(delimiter).map(&:to_i)
+  end
+
+  def validate_negatives(nums)
+    negatives = nums.select(&:negative?)
+    raise "negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
+  end
+
+  def filter_large_numbers(nums)
+    nums.reject { |n| n > 1000 }
+  end
+
   def parse_custom_delimiter(input)
-    # Check for multiple delimiters inside square brackets (e.g., //[*][%]\n1*2%3)
     if input.start_with?("//[")
-      match = input.match(%r{//\[(.+?)\]\n(.*)}) # Match multiple delimiters
-      delimiters = match[1].split('][').map { |delim| Regexp.escape(delim) }.join('|') # Create a regex for all delimiters
-      [delimiters, match[2]]
+      match = input.match(%r{//\[(.+?)\]\n(.*)})
+      delimiters = match[1].split('][').map { |delim| Regexp.escape(delim) }.join('|')
+      [Regexp.new(delimiters), match[2]]
     else
       match = input.match(%r{//(.)\n(.*)})
-      [Regexp.escape(match[1]), match[2]]
+      [Regexp.new(Regexp.escape(match[1])), match[2]]
     end
   end
 end
