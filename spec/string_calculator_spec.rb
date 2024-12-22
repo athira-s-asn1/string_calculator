c47e8ccd59d35e3a6688264a3c4e540fae2c898c
athira-s-asn1 <asnathira@gmail.com>
2024-12-22 18:47:44 +0530

    Added test for invalid numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,8 +6,12 @@ RSpec.describe StringCalculator do
   subject(:calculator) { described_class.new }
 
   describe '#add' do
-    it 'ignores numbers greater than 1000' do
-        expect(calculator.add('2,1001')).to eq(2) # Expected output
+    it 'returns 1000 for a number equal to 1000' do
+      expect(calculator.add('1000')).to eq(1000)
+    end
+      
+    it 'returns 0 for a string with no valid numbers' do
+      expect(calculator.add('abc')).to eq(0)
     end
   end
 end
